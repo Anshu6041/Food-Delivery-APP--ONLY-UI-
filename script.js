cf791704580ac985eefe756461bf5cab847133de
Anshu6041 <161314024+Anshu6041@users.noreply.github.com>
2024-03-11 20:02:25 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,17 @@
+// const left = document.querySelector('.left');
+// const right = document.querySelector('.right');
+// const slider = document.querySelector('.slider');
+// const image7 = document.querySelectorAll('.image7')
+
+// let slideNumber = 1;
+
+// right.addEventListener('click', () => {
+//     if(slideNumber<image7.length){
+//     slider.style.transform = `translateX(-${slideNumber * 800}px)`;
+//     slideNumber++;
+//     }
+//     else{
+//         slider.style.transform = `translateX(-${slideNumber * 0}px)`;
+//         slideNumber=1;
+//     }
+// });
